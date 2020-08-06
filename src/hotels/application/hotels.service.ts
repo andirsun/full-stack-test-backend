d6ae9471b3de6fba94cc8a24fe1238cf6d59dca6
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-08-06 16:54:28 -0500

    feat(Hotel) : new Create Hotel endpoint

--- src/hotels/application/hotels.service.ts
+++ src/hotels/application/hotels.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+// Mongoose dependencies
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IHotel } from '../domain/interfaces/hotel.interface';
+// Dtos
+import { CreateHotelDTO } from '../domain/dto/hotel.dto';
 
 @Injectable()
-export class HotelsService {}
+export class HotelsService {
+  
+  constructor(
+    @InjectModel('Hotels') private readonly hotelModel : Model<IHotel>,
+  ){}
+
+  /*
+    THis function create a Hotel. you need to pass the
+    CreateHotelDto object to works
+  */
+  async createHotel(createHotelDTO : CreateHotelDTO) : Promise<IHotel>{
+    //create the user model with all parameters from interface
+    let hotel = new this.hotelModel(createHotelDTO);
+    //Add the current date and registration method
+    return await hotel.save();
+  }
+}
--- src/hotels/infrastructure/hotels.controller.ts
+++ src/hotels/infrastructure/hotels.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Res, Body, HttpStatus } from '@nestjs/common';
+import { HotelsService } from '../application/hotels.service';
+import { CreateHotelDTO } from '../domain/dto/hotel.dto';
 
 @Controller('hotels')
-export class HotelsController {}
+export class HotelsController {
+  
+  constructor(
+    private hotelService : HotelsService
+  ){}
+  
+  /*
+    Endpoint to create a new hotel
+  */
+  @Post('/new')
+  async createHotel(@Res() res,@Body() createHotelDTO : CreateHotelDTO ){
+    // Using the hotel service to make the used db insertion
+    this.hotelService.createHotel(createHotelDTO)
+      .then(hotel =>{
+        return res.status(HttpStatus.OK).json({
+          content:{
+            hotel
+          }
+        })
+      })
+      .catch(err =>{
+        res.status(HttpStatus.BAD_REQUEST).json({
+          content : {
+            message: 'Ups! Ha ocurrido un error'
+          }
+        })
+        throw new Error(err);
+      });
+  }
+}
