4cdba14521ccd802464a0d44cd5bd5072d6e9b42
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-08-06 09:46:55 -0500

    feat(Database): new database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+//Mongoose ORM Database Module COnection
+import { MongooseModule } from "@nestjs/mongoose";
+// .env dependencies
+//import { ConfigModule } from '@nestjs/config';
+require("dotenv").config();
 
 @Module({
-  imports: [],
+  imports: [
+    /* .env variables */
+    // ConfigModule.forRoot({
+    //   envFilePath : '.env',
+    //   isGlobal : true
+    // }),
+    /* Databases Modules */ 
+    MongooseModule.forRoot(process.env.DB_PROD , {
+      connectionName : 'TocomaDB',
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
