7d331cd0f584b8a8158d44e10af6216f83b7645b
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-08-06 16:54:16 -0500

    feat(Hotel) : new hotel collection

--- src/hotels/infrastructure/hotels.module.ts
+++ src/hotels/infrastructure/hotels.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { HotelsService } from '../application/hotels.service';
 import { HotelsController } from './hotels.controller';
+// External Schemas
+import { HotelSchema } from './schemas/hotel.shema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports :[
+    MongooseModule.forFeature(
+      /*
+        Fist paremeter : array with the schemas to save in the database
+        the second parameter : name database 
+      */
+      [ 
+        //If we need more schemas for user module can import here
+        {name:"Hotels",schema : HotelSchema}
+      ],'TocomaDB'),
+  ],
   providers: [HotelsService],
   controllers: [HotelsController]
 })
