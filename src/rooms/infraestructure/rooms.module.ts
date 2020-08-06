82d5aff5caa601eda6efa1e4af4a584f6af00bb4
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-08-06 13:52:36 -0500

    feat(Rooms) : new database colection conection

--- src/rooms/infraestructure/rooms.module.ts
+++ src/rooms/infraestructure/rooms.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { RoomsService } from '../application/rooms.service';
 import { RoomsController } from './rooms.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+// DB Schemas
+import { RoomSchema } from "./schemas/rooms.schema";
 
 @Module({
+  imports :[
+    MongooseModule.forFeature(
+      /*
+        Fist paremeter : array with the schemas to save in the database
+        the second parameter : name database 
+      */
+      [ 
+        //If we need more schemas for user module can import here
+        {name:"Rooms",schema : RoomSchema}
+      ],'TocomaDB'),
+    // Other module is required need to import here
+  ],
   providers: [RoomsService],
   controllers: [RoomsController]
 })
